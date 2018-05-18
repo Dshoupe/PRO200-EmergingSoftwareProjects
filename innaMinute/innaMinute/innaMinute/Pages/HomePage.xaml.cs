6a033bd4ea1df4664b83271d63b237a324651e26
Chrstian Moore <CMoore@student.neumont.edu>
2018-05-18 14:08:04 -0600

    Made the Homepage so and changed the directory of the models so they are in the innaMinute project instead of innaMinute.Andriod

---
+++ innaMinute/innaMinute/innaMinute/Pages/HomePage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace innaMinute.Pages
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class HomePage : ContentPage
+	{
+		public HomePage ()
+		{
+			InitializeComponent ();
+
+		}
+
+		private void RecordButtonTapGestureRecognizer_Tapped(object sender, EventArgs e)
+		{
+			
+		}
+	}
+}
