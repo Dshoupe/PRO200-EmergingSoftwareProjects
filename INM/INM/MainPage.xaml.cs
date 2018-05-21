efb443e49948799364b5e18f2c846dd48160017b
DShoupe <dylanshoupe@yahoo.com>
2018-05-21 13:15:46 -0600

    Added dummy login

--- INM/INM/MainPage.xaml.cs
+++ INM/INM/MainPage.xaml.cs
@@ -8,22 +8,25 @@ using Xamarin.Forms;
 
 namespace INM
 {
-	public partial class MainPage : ContentPage
-	{
-		public MainPage()
-		{
-			InitializeComponent();
-		}
+				public partial class MainPage : ContentPage
+				{
+								public MainPage()
+								{
+												InitializeComponent();
+								}
 
-		private void CreateAccountBtn_Clicked(object sender, EventArgs e)
-		{
-			this.Navigation.PushAsync(new CreateAccountPage());
-		}
+								private void CreateAccountBtn_Clicked(object sender, EventArgs e)
+								{
+												Navigation.PushAsync(new CreateAccountPage());
+								}
 
-		private void LoginBtn_Clicked(object sender, EventArgs e)
-		{
-			DisplayAlert("Test", $"{EmailEntry.Text} - {PasswordEntry.Text}", "Cancel");
-			Navigation.PushAsync(new HomePage());
-		}
-	}
+								private void LoginBtn_Clicked(object sender, EventArgs e)
+								{
+												//DisplayAlert("Test", $"{EmailEntry.Text} - {PasswordEntry.Text}", "Cancel");
+												if (EmailEntry.Text.ToLower() == "test" && PasswordEntry.Text == "123456")
+												{
+																Navigation.PushAsync(new HomePage());
+												}
+								}
+				}
 }
