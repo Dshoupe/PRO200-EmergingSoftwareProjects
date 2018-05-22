1237000618e5c400f41570fc42ebce3fc856b2d2
Vohnkar <KStringer@student.neumont.edu>
2018-05-22 14:11:42 -0600

    DB Connection
    Begun creation of DB connection files.

---
+++ INM/INM/Database/UsersDataAccess.cs
@@ -0,0 +1,86 @@
+﻿using SQLite;
+using System.Linq;
+using Xamarin.Forms;
+using System.Collections.ObjectModel;
+using INM.Models;
+using static INM.MainPage;
+
+namespace INM.Database
+{
+    public class UsersDataAccess
+    {
+        private SQLiteConnection Database;
+        private static object CollisionLock = new object();
+        public ObservableCollection<User> Users { get; set; }
+
+        public UsersDataAccess()
+        {
+            Database = DependencyService.Get<IDatabaseConnection>().DbConnection();
+            Database.CreateTable<User>();
+            this.Users = new ObservableCollection<User>(Database.Table<User>());
+            if (!Database.Table<User>().Any())
+            {
+                AddNewUser();
+            }
+        }
+
+        private void AddNewUser()
+        {
+            Users.Add(new User
+            {
+                Username = "Default",
+                Email = "empty",
+                FirstName = "John",
+                LastName = "Doe",
+                
+            });
+        }
+
+        public User Login(string username, string password)
+        {
+            lock (CollisionLock)
+            {
+                return Database.Table<User>().FirstOrDefault(user => user.Email == username);
+            }
+        }
+
+        public User GetUser(int id)
+        {
+            lock (CollisionLock)
+            {
+                return Database.Table<User>().FirstOrDefault(user => user.ID == id);
+            }
+        }
+
+        public int SaveUser(User user)
+        {
+            lock (CollisionLock)
+            {
+                if (user.ID != 0)
+                {
+                    Database.Update(user);
+                    return user.ID;
+                }
+                else
+                {
+                    Database.Insert(user);
+                    return user.ID;
+                }
+            }
+        }
+
+        public int DeleteCustomer(User user)
+        {
+            var id = user.ID;
+            if (id != 0)
+            {
+                lock (CollisionLock)
+                {
+                    Database.Delete<User>(id);
+                }
+            }
+            Users.Remove(user);
+            return id;
+        }
+    }
+}
--- INM/INM/MainPage.xaml.cs
+++ INM/INM/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using INM.Pages;
+﻿using INM.Database;
+using INM.Models;
+using INM.Pages;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,25 +10,31 @@ using Xamarin.Forms;
 
 namespace INM
 {
-				public partial class MainPage : ContentPage
-				{
-								public MainPage()
-								{
-												InitializeComponent();
-								}
+    public partial class MainPage : ContentPage
+    {
+        public User CurrentUser { get; set; }
+        public UsersDataAccess UDA { get; set; } = new UsersDataAccess();
+        public MainPage()
+        {
+            InitializeComponent();
+        }
 
-								private void CreateAccountBtn_Clicked(object sender, EventArgs e)
-								{
-												Navigation.PushAsync(new CreateAccountPage());
-								}
+        private void CreateAccountBtn_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new CreateAccountPage());
+        }
 
-								private void LoginBtn_Clicked(object sender, EventArgs e)
-								{
-												//DisplayAlert("Test", $"{EmailEntry.Text} - {PasswordEntry.Text}", "Cancel");
-												if (EmailEntry.Text.ToLower() == "test" && PasswordEntry.Text == "123456")
-												{
-																Navigation.PushAsync(new HomePage());
-												}
-								}
-				}
+        private void LoginBtn_Clicked(object sender, EventArgs e)
+        {
+            //DisplayAlert("Test", $"{EmailEntry.Text} - {PasswordEntry.Text}", "Cancel");
+            if (EmailEntry.Text.ToLower() == "test" && PasswordEntry.Text == "123456")
+            {
+                Navigation.PushAsync(new HomePage());
+            }
+        }
+        public interface IDatabaseConnection
+        {
+            SQLite.SQLiteConnection DbConnection();
+        }
+    }
 }
