25874eac3ed8964c1fbe63f3d0eee3ab86a1a9f8
DShoupe <dylanshoupe@yahoo.com>
2018-05-23 13:59:16 -0600

    Added recording and play button

--- INM/INM.Android/MainActivity.cs
+++ INM/INM.Android/MainActivity.cs
@@ -6,22 +6,24 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Android.Support.V4.App;
+using Android;
 
 namespace INM.Droid
 {
-    [Activity(Label = "INM", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
-    {
-        protected override void OnCreate(Bundle bundle)
-        {
-            TabLayoutResource = Resource.Layout.Tabbar;
-            ToolbarResource = Resource.Layout.Toolbar;
+				[Activity(Label = "INM", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+				public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+				{
+								protected override void OnCreate(Bundle bundle)
+								{
+												TabLayoutResource = Resource.Layout.Tabbar;
+												ToolbarResource = Resource.Layout.Toolbar;
 
-            base.OnCreate(bundle);
+												base.OnCreate(bundle);
 
-            global::Xamarin.Forms.Forms.Init(this, bundle);
-            LoadApplication(new App());
-        }
-    }
+												global::Xamarin.Forms.Forms.Init(this, bundle);
+												LoadApplication(new App());
+								}
+				}
 }
 
--- INM/INM/Pages/HomePage.xaml.cs
+++ INM/INM/Pages/HomePage.xaml.cs
@@ -1,26 +1,78 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿using Android.Media;
+using Java.IO;
+using Java.Lang;
+using System;
+using System.IO;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace INM.Pages
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class HomePage : ContentPage
-	{
-		public HomePage ()
-		{
-			InitializeComponent ();
-		}
+				[XamlCompilation(XamlCompilationOptions.Compile)]
+				public partial class HomePage : ContentPage
+				{
+								MediaRecorder recorder = null;
+								bool recordClicked = false;
+								bool playClicked = false;
+								public HomePage()
+								{
+												InitializeComponent();
+								}
+								
+								private void RecordButton_Tapped(object sender, EventArgs e)
+								{
+												if (!recordClicked)
+												{
+																if (recorder == null)
+																{
+																				recorder = new MediaRecorder();
+																}
+																else
+																{
+																				recorder.Reset();
+																}
 
-		private void RecordButton_Tapped(object sender, EventArgs e)
-		{
+																recorder.SetAudioSource(AudioSource.Mic);
+																recorder.SetOutputFormat(OutputFormat.Mpeg4);
+																recorder.SetAudioEncoder(AudioEncoder.AmrNb);
+																recorder.SetOutputFile(Android.OS.Environment.ExternalStorageDirectory.AbsolutePath + "/test.mp3");
+																recorder.Prepare();
+																recorder.Start();
+																recordClicked = true;
+												}
+												else
+												{
+																recorder.Stop();
+																recorder.Release();
+																recorder = null;
+																recordClicked = false;
+												}
+								}
 
-
-		}
-	}
+								private void PlayTestBtn_Clicked(object sender, EventArgs e)
+								{
+												if(!recordClicked)
+												{
+																MediaPlayer mp = new MediaPlayer();
+																if (!playClicked)
+																{
+																				mp.Reset();
+																				mp.SetDataSource(Android.OS.Environment.ExternalStorageDirectory.AbsolutePath + "/test.mp3");
+																				mp.Prepare();
+																				mp.Start();
+																				playClicked = true;
+																}
+																else
+																{
+																				mp.Stop();
+																				mp.Release();
+																				playClicked = false;
+																}
+												}
+												else
+												{
+																DisplayAlert("Error", "Cannot play while recording", "Ok");
+												}
+								}
+				}
 }
