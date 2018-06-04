e929f23b2c7e53f52d70f515d5cc9afe9e4f1103
DShoupe <dylanshoupe@yahoo.com>
2018-06-03 18:52:43 -0600

    Fixed the MediaRecorder issue

--- INM/INM/Pages/HomePage.xaml.cs
+++ INM/INM/Pages/HomePage.xaml.cs
@@ -14,13 +14,10 @@ namespace INM.Pages
 				[XamlCompilation(XamlCompilationOptions.Compile)]
 				public partial class HomePage : ContentPage
 				{
-								MediaRecorder recorder = new MediaRecorder();
 								User user;
 								bool recordClicked = false;
-								bool playClicked = false;
 								Time time = new Time();
-								string test = "";
-								IDisposable listener = null;
+								MediaRecorder recorder = new MediaRecorder();
 
 								public HomePage(User user)
 								{
@@ -40,19 +37,14 @@ namespace INM.Pages
 																{
 																				recorder.Reset();
 																}
-
 																recorder.SetAudioSource(AudioSource.Mic);
 																recorder.SetOutputFormat(OutputFormat.Mpeg4);
 																recorder.SetAudioEncoder(AudioEncoder.AmrNb);
 																recorder.SetOutputFile(Android.OS.Environment.ExternalStorageDirectory.AbsolutePath + $"/{DateTime.Now.ToString("MM-dd-yyyy-HH;mm;ss")}.mp3");
 																recorder.Prepare();
 																recorder.Start();
-																recordClicked = true;
 																RecordButton.Source = "stopbuttonimage.png";
-																listener = CrossSpeechRecognition.Current.ContinuousDictation().Subscribe(phrase =>
-																{
-																				test += phrase;
-																});
+																recordClicked = true;
 																Device.StartTimer(new TimeSpan(0, 0, 1), () => { time.Seconds++; timeLabel.Text = time.ToString(); return recordClicked; });
 												}
 												else
@@ -63,88 +55,71 @@ namespace INM.Pages
 
 								private void StopRecording()
 								{
-												RecordButton.Source = "recordbutton.png";
-												recorder.Stop();
-												recorder.Release();
-												recordClicked = false;
-												time.Reset();
-												listener.Dispose();
-												testLabel.Text = test;
+												if (recorder != null)
+												{
+																RecordButton.Source = "recordbutton.png";
+																recorder.Stop();
+																recorder.Release();
+																recorder.Dispose();
+																recorder = null;
+																recordClicked = false;
+																time.Reset();
+												}
 								}
 
-								private void PlayTestBtn_Clicked(object sender, EventArgs e)
-								{
-												if (!recordClicked)
-												{
-																MediaPlayer mp = new MediaPlayer();
-																if (!playClicked)
-																{
-																				mp.Reset();
-																				mp.SetDataSource(Android.OS.Environment.ExternalStorageDirectory.AbsolutePath + "/test.mp3");
-																				mp.Prepare();
-																				mp.Start();
-																				playClicked = true;
-																}
-																else
-																{
-																				mp.Stop();
-																				mp.Release();
-																				playClicked = false;
-																}
-												}
-												else
-												{
-																DisplayAlert("Error", "Cannot play while recording", "Ok");
-												}
-								}
+								//private void PlayTestBtn_Clicked(object sender, EventArgs e)
+								//{
+								//				if (!recordClicked)
+								//				{
+								//								MediaPlayer mp = new MediaPlayer();
+								//								if (!playClicked)
+								//								{
+								//												mp.Reset();
+								//												mp.SetDataSource(Android.OS.Environment.ExternalStorageDirectory.AbsolutePath + "/test.mp3");
+								//												mp.Prepare();
+								//												mp.Start();
+								//												playClicked = true;
+								//								}
+								//								else
+								//								{
+								//												mp.Stop();
+								//												mp.Release();
+								//												playClicked = false;
+								//								}
+								//				}
+								//				else
+								//				{
+								//								DisplayAlert("Error", "Cannot play while recording", "Ok");
+								//				}
+								//}
 
 								private void ContactsToolbarItem_Clicked(object sender, EventArgs e)
 								{
-												try
-												{
-																StopRecording();
-												}
-												catch (Exception) { }
+												StopRecording();
 												Navigation.PushAsync(new ContactsPage(user));
 								}
 
 								private void RecordingsToolbarItem_Clicked(object sender, EventArgs e)
 								{
-												try
-												{
-																StopRecording();
-												}
-												catch (Exception) { }
+												StopRecording();
 												Navigation.PushAsync(new RecordingsPage(user));
 								}
 
 								private void GroupsToolbarItem_Clicked(object sender, EventArgs e)
 								{
-												try
-												{
-																StopRecording();
-												}
-												catch (Exception) { }
+												StopRecording();
 												Navigation.PushAsync(new GroupsPage(user));
 								}
 
 								private void ProfileToolbarItem_Clicked(object sender, EventArgs e)
 								{
-												try
-												{
-																StopRecording();
-												}
-												catch (Exception) { }
+												StopRecording();
 												Navigation.PushAsync(new ProfilePage(user));
 								}
 
 								private void SignOutToolbarItem_Clicked(object sender, EventArgs e)
 								{
-												try
-												{
-																StopRecording();
-												}
-												catch (Exception) { }
+												StopRecording();
 												Navigation.PopToRootAsync();
 								}
 				}
