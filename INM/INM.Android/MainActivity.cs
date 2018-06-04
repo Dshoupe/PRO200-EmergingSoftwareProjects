09b96ceb2c01e777c2fe26697f9d4d1463ce2870
DShoupe <dylanshoupe@yahoo.com>
2018-06-03 21:31:59 -0600

    Tried to get the ACR working but couldn't get a working build

--- INM/INM.Android/MainActivity.cs
+++ INM/INM.Android/MainActivity.cs
@@ -4,20 +4,20 @@ using Android.OS;
 
 namespace INM.Droid
 {
-    [Activity(Label = "INM", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
-    {
-        protected override void OnCreate(Bundle bundle)
-        {
-            TabLayoutResource = Resource.Layout.Tabbar;
-            ToolbarResource = Resource.Layout.Toolbar;
-			
-            base.OnCreate(bundle);
+				[Activity(Label = "INM", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+				public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+				{
+								protected override void OnCreate(Bundle bundle)
+								{
+												TabLayoutResource = Resource.Layout.Tabbar;
+												ToolbarResource = Resource.Layout.Toolbar;
 
-            global::Xamarin.Forms.Forms.Init(this, bundle);	      
+												base.OnCreate(bundle);
 
-			LoadApplication(new App());	        
-        }
-    }
+												global::Xamarin.Forms.Forms.Init(this, bundle);
+
+												LoadApplication(new App());
+								}
+				}
 }
 
